c0aa734a5f9fdd4c36a1dec436e67d1fcf5b8742
diogocav <diogo_c_vieira@hotmail.com>
2022-06-21 22:37:22 -0300

    completado o exercicio 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,32 +1,20 @@
-let patientId = '50';
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+// Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
 
-// Utilize o operador typeof para imprimir qual o tipo das variáveis patientId, isEnrolled, patientInfo e patientEmail. Esse operador retorna qual o tipo de uma variável, objeto, função ou expressão. Exemplo: console.log(typeof patientId) retornará number.
+const nota = 59;
 
-console.log(typeof patientId)
-console.log(typeof isEnrolled)
-console.log(typeof patientInfo)
-console.log(typeof patientEmail)
-console.log(typeof patientAge)
+// Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
 
-// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
-// Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
-// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
-// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+// Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+// Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+// Se a nota for menor que 60, imprima "Você foi reprovada(o)"
 
-const base = 5
-let height = 8
-const area = base * height
+// Crie uma estrutura condicional utilizando o if, else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação.
+// Altere o valor da nota para verificar se as condições que você implementou funcionam;
 
-console.log(base);
-console.log(height);
-console.log(area);
-
-const perimeter = 2 * (base + height)
-
-console.log(perimeter);
+if (nota >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (nota < 80 && nota >= 60) {
+  console.log("Você está na nossa lista de espera");
+} else if (nota < 60) {
+  console.log("Você foi reprovada(o)");
+}
