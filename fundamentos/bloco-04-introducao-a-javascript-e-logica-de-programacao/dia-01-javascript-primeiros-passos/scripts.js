fbc20f018e7a401ecae38ab85c0fed8d9c1b4cb3
diogocav <diogo_c_vieira@hotmail.com>
2022-06-22 14:00:20 -0300

    completado exercicio 1.9

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -134,11 +134,24 @@
 // Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
 // Bonus: use somente um if.
 
-const a = 3;
-const b = 4;
-const c = 9;
+// const a = 3;
+// const b = 4;
+// const c = 9;
 
-if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+// if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+const a = 6;
+const b = 4;
+const c = 2;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
   console.log(true);
 } else {
   console.log(false);
