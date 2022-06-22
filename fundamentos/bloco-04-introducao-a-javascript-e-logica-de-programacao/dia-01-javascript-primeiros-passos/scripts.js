85e36ca7a71c2cb71941e00ed505511cdf15f414
diogocav <diogo_c_vieira@hotmail.com>
2022-06-22 11:11:58 -0300

    completado exercicio 1.5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -43,12 +43,28 @@
 
 // Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
-const a = 1;
+// const a = 1;
 
-if (a > 0) {
-  console.log("positive");
-} else if (a < 0) {
-  console.log("negative");
+// if (a > 0) {
+//   console.log("positive");
+// } else if (a < 0) {
+//   console.log("negative");
+// } else {
+//   console.log("zero");
+// }
+
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+
+const a = 90;
+const b = 40;
+const c = -1;
+
+if (a + b + c === 180) {
+  console.log(true);
+} else if (a < 0 || b < 0 || c < 0) {
+  console.log("erro");
 } else {
-  console.log("zero");
+  console.log(false);
 }
