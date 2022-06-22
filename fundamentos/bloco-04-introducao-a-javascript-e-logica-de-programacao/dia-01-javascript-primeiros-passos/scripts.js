498655e90633f9f116a2a631fdad5fdd050618c9
diogocav <diogo_c_vieira@hotmail.com>
2022-06-22 10:54:48 -0300

    completado exercicio 1.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -5,11 +5,25 @@
 // Divisão (a / b)
 // Módulo (a % b)
 
-const a = 5
-const b = 6
+// const a = 5
+// const b = 6
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 30;
+const b = 50;
+
+if (a > b) {
+  console.log(a);
+} else if (b > a) {
+  console.log(b);
+} else {
+  console.log("valores iguais");
+}
+
