1b8bf92ad1623fb5ccd9bcf69705ece3e4575bcd
diogocav <diogo_c_vieira@hotmail.com>
2022-06-21 23:13:07 -0300

    completado exercicio 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,20 +1,38 @@
-// Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
+// Crie uma constante chamada "currentHour" que recebe um número entre 4 e 24 de sua escolha, para representar as horas do dia.
 
-const nota = 59;
+const currentHour = 13;
 
-// Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
+// Crie uma variável chamada "message" que, inicialmente, é uma string vazia.
 
-// Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
-// Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
-// Se a nota for menor que 60, imprima "Você foi reprovada(o)"
+let message = "";
 
-// Crie uma estrutura condicional utilizando o if, else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação.
-// Altere o valor da nota para verificar se as condições que você implementou funcionam;
+// Implemente condicionais para que:
+// Se o horário for maior ou igual a 22, insira "Não deveríamos comer nada, é hora de dormir" na variável "message".
 
-if (nota >= 80) {
-  console.log("Parabéns, você foi aprovada(o)!");
-} else if (nota < 80 && nota >= 60) {
-  console.log("Você está na nossa lista de espera");
-} else if (nota < 60) {
-  console.log("Você foi reprovada(o)");
+if (currentHour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir";
 }
+
+// Se o horário for maior ou igual a 18 e menor que 22, insira "Rango da noite, vamos jantar :D" na variável "message".
+else if (currentHour >= 18 && currentHour < 22) {
+  message = "Rango da noite, vamos jantar :D";
+}
+
+// Se o horário for maior ou igual a 14 e menor que 18, insira "Vamos fazer um bolo pro café da tarde?" na variável "message".
+else if (currentHour >= 14 && currentHour < 18) {
+  message = "Vamos fazer um bolo pro café da tarde?";
+}
+
+// Se o horário estiver entre 11 e 14, insira "Hora do almoço!!!" na variável "message".
+else if (currentHour > 11 && currentHour < 14) {
+  message = "Hora do almoço!!";
+}
+
+// Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado" na variável "message".
+else if (currentHour > 4 && currentHour < 11) {
+  message = "Hmmm, cheiro de café recém passado";
+}
+
+// Agora imprima a variável message fora das suas condições.
+
+console.log(message);
