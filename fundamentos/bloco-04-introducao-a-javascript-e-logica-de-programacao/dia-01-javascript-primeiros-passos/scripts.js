c3f4a9753bebc7db21e7cb8a891c18075b3fa73a
diogocav <diogo_c_vieira@hotmail.com>
2022-06-22 11:01:26 -0300

    completado exercicio 1.4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -29,14 +29,26 @@
 
 // Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
-const a = 3;
-const b = 6;
-const c = 9;
+// const a = 3;
+// const b = 6;
+// const c = 9;
 
-if (a > b && a > c) {
-  console.log(a);
-} else if (b > a && b > c) {
-  console.log(b);
-} else if (c > a && c > b) {
-  console.log(c);
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > a && b > c) {
+//   console.log(b);
+// } else if (c > a && c > b) {
+//   console.log(c);
+// }
+
+// Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const a = 1;
+
+if (a > 0) {
+  console.log("positive");
+} else if (a < 0) {
+  console.log("negative");
+} else {
+  console.log("zero");
 }
