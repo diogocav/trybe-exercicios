0d0138317c28ce5becf6c0c9ad0bf4a94d7f6f6c
diogocav <diogo_c_vieira@hotmail.com>
2022-06-23 11:55:35 -0300

    completado exe 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -8,13 +8,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
-let soma = 0
-for (let i = 0; i < numbers.length; i += 1){
-    soma = numbers[i] + soma
-}
-console.log(soma);
+// let soma = 0
+// for (let i = 0; i < numbers.length; i += 1){
+//     soma = numbers[i] + soma
+// }
+// console.log(soma);
 
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+  soma = numbers[i] + soma;
+}
+let mediaArit = soma / numbers.length
+console.log(mediaArit);
+
 // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 // 🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
