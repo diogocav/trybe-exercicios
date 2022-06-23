31601cf458ac34ab3e956155fce75ff8d75b93a1
diogocav <diogo_c_vieira@hotmail.com>
2022-06-23 20:53:39 -0300

    completado exe 7

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -51,14 +51,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-let impar = 0;
-for (let i = 0; i < numbers.length; i += 1) {
-  if (numbers[i] % 2 !== 0) {
-    impar += 1;
-  }
-}
-console.log(impar);
+// let impar = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 !== 0) {
+//     impar += 1;
+//   }
+// }
+// console.log(impar);
 
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let menor = numbers[0]
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] < menor) {
+    menor = numbers[i]
+  }
+}
+console.log(menor);
+
+
 // Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 // Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
