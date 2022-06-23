ca1070330eeef927f34b32146fafe50c2a271f6d
diogocav <diogo_c_vieira@hotmail.com>
2022-06-23 11:59:51 -0300

    completado exe 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -15,16 +15,32 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(soma);
 
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma = numbers[i] + soma;
+// }
+// let mediaArit = soma / numbers.length
+// console.log(mediaArit);
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 let soma = 0;
 for (let i = 0; i < numbers.length; i += 1) {
   soma = numbers[i] + soma;
 }
 let mediaArit = soma / numbers.length
-console.log(mediaArit);
+if (mediaArit > 20){
+    console.log("valor maior que 20");
 
-// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
-// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+} else {
+    console.log("valor menor ou igual a 20");
+
+}
+
+
+
 // 🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
