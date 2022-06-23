fea9a690655a33a7021e7f0b73a64b842017bd84
diogocav <diogo_c_vieira@hotmail.com>
2022-06-23 12:07:00 -0300

    completado exe 5

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -26,22 +26,30 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-let soma = 0;
-for (let i = 0; i < numbers.length; i += 1) {
-  soma = numbers[i] + soma;
-}
-let mediaArit = soma / numbers.length
-if (mediaArit > 20){
-    console.log("valor maior que 20");
+// let soma = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma = numbers[i] + soma;
+// }
+// let mediaArit = soma / numbers.length
+// if (mediaArit > 20){
+//     console.log("valor maior que 20");
 
-} else {
-    console.log("valor menor ou igual a 20");
+// } else {
+//     console.log("valor menor ou igual a 20");
 
-}
-
-
+// }
 
 // 🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+
+let maiorN = 0
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > maiorN){
+        maiorN = numbers[i]
+    }
+}
+console.log(maiorN);
+
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 // Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
