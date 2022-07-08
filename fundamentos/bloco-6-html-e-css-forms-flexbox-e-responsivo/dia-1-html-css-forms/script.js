3de444381241c0c7923637da0f0f7eb247a19d72
diogocav <diogo_c_vieira@hotmail.com>
2022-07-08 11:08:50 -0300

    completado exes 1 e 2

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,21 +1,3 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). Nossa amiga Carol Silva nos contou um pouco sobre como fazer isso, lembra?
+// Crie um botão que limpe as informações contidas nos campos;
 
-HREF_LINK.addEventListener('click', (event) => {
-    event.preventDefault();
-})
-INPUT_CHECKBOX.addEventListener('click', (event) => {
-    event.preventDefault();
-})
-INPUT_TEXT.addEventListener('keypress', (event) => {
-    if (event.key !== 'a')event.preventDefault();
-})
-
-
-
-// 1 - Adicione um evento click no elemento a que possui o id href. Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
-// 2 - Adicione um evento click no elemento checkbox que possui o id input-checkbox. Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
-// 3 - Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra 'a' minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
-// Caso deseje entender melhor o comportamento do método .preventDefault(), acesse a documentação do MDN ou a página da w3schools.
