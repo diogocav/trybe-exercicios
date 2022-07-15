b48dfb6db36600028f26c1b35dc9626518726d72
diogocav <diogo_c_vieira@hotmail.com>
2022-07-15 10:45:14 -0300

    completado exercicio 1

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitario/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,17 @@
+// Modifique a estrutura da função para que ela seja uma arrow function;
+// Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+// Modifique as concatenações para template literals.
+
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    elseScope = `${elseScope}, ótimo, fui utilizada no escopo !`;
+    console.log(elseScope);
+  }
+}
+
+testingScope(false);
