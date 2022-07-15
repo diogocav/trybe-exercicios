8a32a95f58f1dae771ed299b5d4bed0cbef8cdfc
diogocav <diogo_c_vieira@hotmail.com>
2022-07-15 16:00:27 -0300

    completado parte 2 exercicio 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitario/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitario/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,3 +1,5 @@
+// Parte I
+
 // exercicio 1
 // Modifique a estrutura da função para que ela seja uma arrow function;
 // Modifique as variáveis para que respeitem o escopo onde estão declaradas;
@@ -24,22 +26,52 @@
 // Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
 // Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-for (let repetitions = 0; repetitions < oddsAndEvens.length; repetitions++) {
-  for (let index = 0; index < oddsAndEvens.length; index++) {
-    let antes = oddsAndEvens[index];
-    let depois = oddsAndEvens[index + 1];
-    if (antes > depois) {
-      oddsAndEvens[index] = depois;
-      oddsAndEvens[index + 1] = antes;
-    }
-  }
-}
+// for (let repetitions = 0; repetitions < oddsAndEvens.length; repetitions++) {
+//   for (let index = 0; index < oddsAndEvens.length; index++) {
+//     let antes = oddsAndEvens[index];
+//     let depois = oddsAndEvens[index + 1];
+//     if (antes > depois) {
+//       oddsAndEvens[index] = depois;
+//       oddsAndEvens[index + 1] = antes;
+//     }
+//   }
+// }
 
-console.log(oddsAndEvens);
+// console.log(oddsAndEvens);
 
 // oddsAndEvens.sort((a, b) => a - b);
 
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
-console.log('Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!')
+// console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+// console.log('Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!')
+
+
+// Parte II
+
+// Na matemática, o fatorial de um número inteiro e positivo N, representado por N!, é o produto de todos os seus antecessores até o número um. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+const fatorial = numero => {
+    let resultado = 1
+    for (let repeat = 1; repeat <= numero; repeat++) {
+        resultado *= repeat
+        console.log(resultado);
+    }
+    return resultado
+}
+
+console.log(fatorial(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
